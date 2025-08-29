afb3df838f3f3edc63dd258e5a37deeb2f18397f
Ryan Reis Poltronieri <ryanreisunivap@gmail.com>
2025-08-28 22:23:53 -0300

    Add files via upload

---
+++ Exercício 3 Obra 2/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Paisagem paisagem = new Paisagem();
+        paisagem.visualizar("Torre Eiffel");
+        
+        País país = new País();
+        país.viajar("França");
+        
+        Torre torre = new Torre();
+        torre.apreciar(138);
+        
+    }
+    
+}
---
+++ Exercício 3 Obra 2/Torre.java
@@ -0,0 +1,14 @@
+
+public class Torre {
+    
+    private String material;
+    private int altura;
+    private int idade;
+    
+    public static void apreciar(int idade) {
+        
+        System.out.println("A Torre Eiffel tem " + idade + " anos");
+        
+    }
+    
+}
