65436449c04cf553d659fbb8aabbf99e9c4d5e6a
Ryan Reis Poltronieri <ryanreisunivap@gmail.com>
2025-08-28 22:01:04 -0300

    Add files via upload

---
+++ Exercício 3 Obra 1/Cantor.java
@@ -0,0 +1,14 @@
+
+public class Cantor {
+    
+    private String nome;
+    private int idade;
+    private String país;
+    
+    public void exibir(String nome, int idade, String país) {
+        
+        System.out.println(nome + " era um cantor de " + idade + " anos nascido na " + país);
+        
+    }
+    
+}
