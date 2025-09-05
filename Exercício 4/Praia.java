ddbe37a92fac7a339130adf42673f591c0720c15
Ryan Reis Poltronieri <ryanreisunivap@gmail.com>
2025-09-05 20:21:25 -0300

    Update Praia.java

--- Exercício 4/Praia.java
+++ Exercício 4/Praia.java
@@ -6,9 +6,17 @@ public class Praia {
     private String ondas;
     
     public void relaxar(String mar, String ondas) {
-        
         System.out.println("O mar estava " + mar + " e as ondas estavam " + ondas + " !");
-        
+        }
+    public void curtir(String nome){
+        System.out.println("O " +nome+ " estava curtindo na praia!");
     }
+    public void nadar(String f){
+        System.out.println("O mar estava agitado, mas estava " +f+ " para nadar!");
+        System.out.println("----------------------------------------------------------");
+    }
+    
+    
+    
     
 }
