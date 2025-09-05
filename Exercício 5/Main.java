bf50d7846e6da700890a79198b43409d97a8502a
Ryan Reis Poltronieri <ryanreisunivap@gmail.com>
2025-09-05 20:33:25 -0300

    Update Main.java

--- Exercício 5/Main.java
+++ Exercício 5/Main.java
@@ -1,15 +1,22 @@
+
 public class Main {
 
 
     public static void main(String[] args) {
         Personagem personagem = new Personagem();
         personagem.defender("Ryan" , "canhão");
+        personagem.atacar("inimigos");
+        personagem.controlar("partida");
     
         CampoDeBatalha campodebatalha = new CampoDeBatalha();
         campodebatalha.destruir("Bárbaros" , "inimiga");
+        campodebatalha.assistir("jogar");
+        campodebatalha.torcer("ganhar");
     
         Torre torre = new Torre();
         torre.derrubarInimigos("Rei", "canhão");
+        torre.atirar("inimigos");
+        torre.defender("destruída");
     
     
     
