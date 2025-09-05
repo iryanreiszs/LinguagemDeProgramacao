560109025a4d95b1108e30dc453ba2ce3c883401
Ryan Reis Poltronieri <ryanreisunivap@gmail.com>
2025-09-05 20:20:21 -0300

    Update Baleia.java

--- Exercício 4/Baleia.java
+++ Exercício 4/Baleia.java
@@ -8,4 +8,18 @@ public class Baleia {
         System.out.println("A Baleia " + tipo + " está quase em extinção!");
     }
     
+    public void respirar(String jeito){
+        System.out.println("As baleias respiram pelo " +jeito+ "!");
+    }
+        
+    public void comer(String p){
+        System.out.println("As baleias se alimentam de " +p+ "!");
+        
+        
+        
+        
+        
+        System.out.println("----------------------------------------------------------");
+    }
+   
 }
