86240e5c62a406c6a07dc2ac975e52be75ee47d4
Ryan Reis Poltronieri <ryanreisunivap@gmail.com>
2025-09-05 20:34:05 -0300

    Update Torre.java

--- Exercício 5/Torre.java
+++ Exercício 5/Torre.java
@@ -9,4 +9,12 @@ public class Torre {
         System.out.println("O " + pessoa + " utiliza seu " + arma + " para neutralizar seus inimigos!" );
     }
     
+    public void atirar(String a){
+        System.out.println("A torre neutraliza os" +a+ "!");
+    }
+    
+    public void defender(String d){
+        System.out.println("A torre se defende para nao ser" +d+ "!");
+        System.out.println("---------------------------------");
+    }
 }
