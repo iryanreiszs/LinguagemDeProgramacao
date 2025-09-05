a04a1c642ee93163001546f070b42a2e83c479eb
Ryan Reis Poltronieri <ryanreisunivap@gmail.com>
2025-09-05 20:33:43 -0300

    Update CampoDeBatalha.java

--- Exercício 5/CampoDeBatalha.java
+++ Exercício 5/CampoDeBatalha.java
@@ -6,9 +6,15 @@ public class CampoDeBatalha {
      private String personagens;
      
      public void destruir(String personagens, String torre) {
-        
          System.out.println("Os " + personagens + " vão em direção à torre " + torre + " para atacar!");
-         
+    }
+     
+    public void assistir(String a){
+        System.out.println("Os bonecos assistem o " +a+ "!");
+    }
+    public void torcer(String g){
+        System.out.println("Os personagens torcem para alguém" +g+ "!");
+        System.out.println("---------------------------------");
     }
     
 } 
