d2fa3b8a6b024c897bcdc0f483109fdd5982328f
Ryan Reis Poltronieri <ryanreisunivap@gmail.com>
2025-09-05 20:20:35 -0300

    Update Astronauta.java

--- Exercício 4/Astronauta.java
+++ Exercício 4/Astronauta.java
@@ -7,5 +7,16 @@ public class Astronauta {
     public void viajar(String x) {
         System.out.println("O astronauta teve como objetivo " + x + " no planeta!");
     }
-    
+    public void pousarNaLua(String nome){
+        System.out.println("O astronauta " +nome+ " conseguiu subir na lua!");
+    }
+    public void reabastecerNave(String n){
+        System.out.println("O astronauta " +n+ " conseguiu abastecer sua nave!");
+        System.out.println("----------------------------------------------------------");
+    }
+        
+        
 }
+    
+    
+
