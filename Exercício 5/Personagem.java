b2e05a69aedadec930f956c8490df3e3abc9b6bd
Ryan Reis Poltronieri <ryanreisunivap@gmail.com>
2025-09-05 20:33:54 -0300

    Update Personagem.java

--- Exercício 5/Personagem.java
+++ Exercício 5/Personagem.java
@@ -7,5 +7,11 @@ public class Personagem {
     public void defender(String nome, String arma) {
         System.out.println("O Rei " + nome + " usa o " + arma + " para defender sua torre!");
     }
-    
+    public void atacar(String i){
+        System.out.println("O Rei ataca os " +i+ "!");
+    }
+    public void controlar(String p){
+        System.out.println("O Rei controla a " +p+ "!");
+        System.out.println("---------------------------------");
+    }
 }
