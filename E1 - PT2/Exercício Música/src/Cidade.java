42f6616f4704f566bcc3d57ba79b67b114c5cb03
Ryan Reis Poltronieri <ryanreisunivap@gmail.com>
2025-09-23 22:30:28 -0300

    Update Cidade.java

--- E1 - PT2/Exercício Música/src/Cidade.java
+++ E1 - PT2/Exercício Música/src/Cidade.java
@@ -1,12 +1,36 @@
 public class Cidade {
 
     private String lugar;
-    private String populacao;
+    private int populacao;
     private String pessoa;
 
-    public Cidade(String lugar, String populacao, String pessoa) {
+    public Cidade(String lugar, int populacao, String pessoa) {
         this.lugar = lugar;
         this.populacao = populacao;
+        this.pessoa = pessoa;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    public int getPopulacao() {
+        return populacao;
+    }
+
+    public void setPopulacao(int populacao) {
+        this.populacao = populacao;
+    }
+
+    public String getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(String pessoa) {
         this.pessoa = pessoa;
     }
 
