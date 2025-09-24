bf44923cf4aefeee3bf0da137133d4c26c486294
Ryan Reis Poltronieri <ryanreisunivap@gmail.com>
2025-09-23 22:28:58 -0300

    Update Pessoa.java

--- E1 - PT2/Exercício Música/src/Pessoa.java
+++ E1 - PT2/Exercício Música/src/Pessoa.java
@@ -10,6 +10,30 @@ public class Pessoa {
         this.apelido = apelido;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
     public void apresentar() {
         System.out.println("Olá, meu nome é " + nome + " (" + apelido + "), e tenho " + idade + " anos.");
     }
