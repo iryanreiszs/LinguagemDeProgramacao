cad0c46d92bfbe0e8fa96b19600db30a3510ab5b
Ryan Reis Poltronieri <ryanreisunivap@gmail.com>
2025-09-23 22:29:59 -0300

    Update Comida.java

--- E1 - PT2/Exercício Música/src/Comida.java
+++ E1 - PT2/Exercício Música/src/Comida.java
@@ -1,11 +1,35 @@
 public class Comida {
     private String nome;
     private String tipo;
-    private String preco;
+    private int preco;
 
-    public Comida(String nome, String tipo, String preco) {
+    public Comida(String nome, String tipo, int preco) {
         this.nome = nome;
         this.tipo = tipo;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getPreco() {
+        return preco;
+    }
+
+    public void setPreco(int preco) {
         this.preco = preco;
     }
 
